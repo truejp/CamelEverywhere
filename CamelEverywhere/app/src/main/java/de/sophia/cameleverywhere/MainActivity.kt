066f4971b5d0e3aa4307b56dec53b78d9f94b4b9
sophia-sturm <90406125+sophia-sturm@users.noreply.github.com>
2021-10-22 10:20:04 +0200

    :tada: initial commit

---
+++ CamelEverywhere/app/src/main/java/de/sophia/cameleverywhere/MainActivity.kt
@@ -0,0 +1,13 @@
+package de.sophia.cameleverywhere
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
