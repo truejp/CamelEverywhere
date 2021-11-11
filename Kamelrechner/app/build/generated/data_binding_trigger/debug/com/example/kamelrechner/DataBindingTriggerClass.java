846a81da1e19afe4f983d2f16d284e5b3efd3b65
Philipp Lehnet <80537345+truejp@users.noreply.github.com>
2021-11-11 15:31:20 +0100

    Delete Kamelrechner directory

--- Kamelrechner/app/build/generated/data_binding_trigger/debug/com/example/kamelrechner/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.kamelrechner;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- Kamelrechner/app/build/generated/source/kapt/debug/com/example/kamelrechner/BR.java
+++
@@ -1,5 +0,0 @@
-package com.example.kamelrechner;
-
-public class BR {
-  public static final int _all = 0;
-}
