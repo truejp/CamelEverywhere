28da3c3be2faab0e862f3125baf0115eaf736bb9
truejp <80537345+truejp@users.noreply.github.com>
2021-11-11 15:30:01 +0100

    Refresh

---
+++ Kamelrechner/app/build/generated/data_binding_trigger/debug/com/example/kamelrechner/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.kamelrechner;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
---
+++ Kamelrechner/app/build/tmp/kapt3/stubs/debug/com/example/kamelrechner/Calculator.java
@@ -0,0 +1,39 @@
+package com.example.kamelrechner;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000.\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0012\u0010\u0011\u001a\u00020\u00122\b\u0010\u0013\u001a\u0004\u0018\u00010\u0014H\u0015R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u001a\u0010\u0005\u001a\u00020\u0006X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0007\u0010\b\"\u0004\b\t\u0010\nR\u001c\u0010\u000b\u001a\u0004\u0018\u00010\fX\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\r\u0010\u000e\"\u0004\b\u000f\u0010\u0010\u00a8\u0006\u0015"}, d2 = {"Lcom/example/kamelrechner/Calculator;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "button", "Landroid/widget/Button;", "radioButton", "Landroid/widget/RadioButton;", "getRadioButton", "()Landroid/widget/RadioButton;", "setRadioButton", "(Landroid/widget/RadioButton;)V", "radioGroup", "Landroid/widget/RadioGroup;", "getRadioGroup", "()Landroid/widget/RadioGroup;", "setRadioGroup", "(Landroid/widget/RadioGroup;)V", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
+public final class Calculator extends androidx.appcompat.app.AppCompatActivity {
+    @org.jetbrains.annotations.Nullable()
+    private android.widget.RadioGroup radioGroup;
+    public android.widget.RadioButton radioButton;
+    private android.widget.Button button;
+    
+    public Calculator() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final android.widget.RadioGroup getRadioGroup() {
+        return null;
+    }
+    
+    public final void setRadioGroup(@org.jetbrains.annotations.Nullable()
+    android.widget.RadioGroup p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final android.widget.RadioButton getRadioButton() {
+        return null;
+    }
+    
+    public final void setRadioButton(@org.jetbrains.annotations.NotNull()
+    android.widget.RadioButton p0) {
+    }
+    
+    @android.annotation.SuppressLint(value = {"ResourceType"})
+    @java.lang.Override()
+    protected void onCreate(@org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+}
